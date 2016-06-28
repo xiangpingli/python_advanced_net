fb320d27fc09292887e60cc79e03d782cbbc8874
李祥平 <lixiangping@hikvision.com.cn>
2016-06-27 18:13:43 -0700

    上传settimeout.py

---
+++ settimeout.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+import socket
+
+def test_socket_timeout():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print "Default socket timeout: %s" %s.gettimeout()
+
+    s.settimeout(100.0)
+    print "current socket timeout: %s" %s.gettimeout()
+    
+
+if __name__=="__main__":
+    test_socket_timeout()
